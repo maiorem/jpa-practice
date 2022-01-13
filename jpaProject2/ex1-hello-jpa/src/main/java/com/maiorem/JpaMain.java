aa223bce06b4a24c590ec6d5b90f031394533d08
Seyoung Hong <maiorem00@gmail.com>
2022-01-13 14:32:15 +0900

    jpql

--- jpaProject2/ex1-hello-jpa/src/main/java/com/maiorem/JpaMain.java
+++ jpaProject2/ex1-hello-jpa/src/main/java/com/maiorem/JpaMain.java
@@ -5,6 +5,7 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 import java.beans.PersistenceDelegate;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -21,8 +22,18 @@ public class JpaMain {
 //            em.persist(member);
 
             // update data
-            Member findMember = em.find(Member.class, 1L);
-            findMember.setName("HelloJpa");
+//            Member findMember = em.find(Member.class, 1L);
+//            findMember.setName("HelloJpa");
+
+            // JPQL
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                    .setFirstResult(1)
+                    .setMaxResults(10) //페이징
+                    .getResultList();
+            
+            for (Member member : result) {
+                System.out.println("member.name = " + member.getName());
+            }
 
             tx.commit();
         } catch (Exception e) {
