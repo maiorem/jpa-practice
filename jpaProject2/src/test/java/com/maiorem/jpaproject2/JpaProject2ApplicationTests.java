05583d97e487c3957ff002a1197e31c694f6cc0e
Seyoung Hong <maiorem00@gmail.com>
2022-01-13 10:20:17 +0900

    jpa project init

---
+++ jpaProject2/src/test/java/com/maiorem/jpaproject2/JpaProject2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maiorem.jpaproject2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaProject2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
