2c2554007068b07ee119135863e1eab2e37a19ce
Seyoung Hong <maiorem00@gmail.com>
2022-01-13 14:10:59 +0900

    Member insert

--- jpaProject2/ex1-hello-jpa/src/main/java/com/maiorem/JpaMain.java
+++ jpaProject2/ex1-hello-jpa/src/main/java/com/maiorem/JpaMain.java
@@ -1,11 +1,31 @@
 package com.maiorem;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 import java.beans.PersistenceDelegate;
 
 public class JpaMain {
     public static void main(String[] args) {
-         EntityManagerFactory enf = Persistence.createEntityManagerFactory("hello");
+        EntityManagerFactory enf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = enf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            Member member = new Member();
+
+            member.setId(1L);
+            member.setName("Hello");
+
+            em.persist(member);
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+        enf.close();
     }
 }
