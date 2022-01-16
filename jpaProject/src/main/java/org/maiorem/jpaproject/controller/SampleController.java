59c99e2f618e4bfcdba730a78071bee718b2c96c
Seyoung Hong <maiorem00@gmail.com>
2022-01-16 21:43:01 +0900

    thymeleaf include fragments

--- jpaProject/src/main/java/org/maiorem/jpaproject/controller/SampleController.java
+++ jpaProject/src/main/java/org/maiorem/jpaproject/controller/SampleController.java
@@ -59,7 +59,12 @@ public class SampleController {
 
     @GetMapping("/ex3")
     public void ex3() {
-        log.info("ex3");
+        log.info("ex3......................");
+    }
+
+    @GetMapping({"/exLayout1", "/exLayout2", "/exTemplate"})
+    public void exLayout1() {
+        log.info("exLayout....................");
     }
 
 }
