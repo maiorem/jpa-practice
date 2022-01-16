533550c263fcfff2a64ea8542917e65a39887ee0
Seyoung Hong <maiorem00@gmail.com>
2022-01-16 19:39:55 +0900

    thymeleaf inline and block

--- jpaProject/src/main/java/org/maiorem/jpaproject/controller/SampleController.java
+++ jpaProject/src/main/java/org/maiorem/jpaproject/controller/SampleController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -35,6 +36,30 @@ public class SampleController {
         }).collect(Collectors.toList());
 
         model.addAttribute("list", list);
+    }
+
+    //thymeleaf inline
+    @GetMapping("/exinline")
+    public String exInline(RedirectAttributes redirectAttributes) {
+
+        log.info("exInline.................");
+
+        SampleDTO dto = SampleDTO.builder()
+                .sno(100L)
+                .first("First.......100")
+                .last("Last.........100")
+                .regTime(LocalDateTime.now())
+                .build();
+        redirectAttributes.addFlashAttribute("result", "success");
+        redirectAttributes.addFlashAttribute("dto", dto);
+
+        return "redirect:/sample/ex3";
+
+    }
+
+    @GetMapping("/ex3")
+    public void ex3() {
+        log.info("ex3");
     }
 
 }
