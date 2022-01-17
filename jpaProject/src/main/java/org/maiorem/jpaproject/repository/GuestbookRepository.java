0f08e5abfc65137774889228cce0d93a7939cca0
Seyoung Hong <maiorem00@gmail.com>
2022-01-17 21:29:17 +0900

    Querydsl Setting

--- jpaProject/src/main/java/org/maiorem/jpaproject/repository/GuestbookRepository.java
+++ jpaProject/src/main/java/org/maiorem/jpaproject/repository/GuestbookRepository.java
@@ -2,6 +2,7 @@ package org.maiorem.jpaproject.repository;
 
 import org.maiorem.jpaproject.entity.Guestbook;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
-public interface GuestbookRepository extends JpaRepository<Guestbook, Long> {
+public interface GuestbookRepository extends JpaRepository<Guestbook, Long>, QuerydslPredicateExecutor<Guestbook> {
 }
