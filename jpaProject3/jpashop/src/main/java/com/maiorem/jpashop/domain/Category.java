fc9dda687fedd1d0b5908ff7e79f493de3150081
Seyoung Hong <maiorem00@gmail.com>
2022-02-02 19:33:28 +0900

    계층구조

--- jpaProject3/jpashop/src/main/java/com/maiorem/jpashop/domain/Category.java
+++ jpaProject3/jpashop/src/main/java/com/maiorem/jpashop/domain/Category.java
@@ -26,4 +26,14 @@ public class Category {
             joinColumns = @JoinColumn(name = "category"),
             inverseJoinColumns = @JoinColumn(name = "item_id")) //다대일 일대일로 풀어갈 중간 테이블 매핑
     private List<Item> items = new ArrayList<>();
+
+    //카테고리(계층)구조
+    //부모
+    @ManyToOne
+    @JoinColumn(name = "parent_id")
+    private Category parents;
+
+    //자식
+    @OneToMany(mappedBy = "parent")
+    private List<Category> child = new ArrayList<>();
 }
