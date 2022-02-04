ad9401cbfa9431662232a2d3a53c4d3565e473d0
Seyoung Hong <maiorem00@gmail.com>
2022-02-04 23:05:25 +0900

    member service test

--- jpaProject3/jpashop/src/main/java/com/maiorem/jpashop/repository/MemberRepository.java
+++ jpaProject3/jpashop/src/main/java/com/maiorem/jpashop/repository/MemberRepository.java
@@ -1,24 +1,41 @@
 package com.maiorem.jpashop.repository;
 
 import com.maiorem.jpashop.domain.Member;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.List;
 
 @Repository
+@RequiredArgsConstructor
 public class MemberRepository {
 
-    @PersistenceContext
-    private EntityManager em;
+    // @PersistenceContext
+    private final EntityManager em;
 
-    public Long save(Member member) {
+    //public MemberRepository(EntityManager em) {
+    //    this.em = em;
+    //}
+
+    public void save(Member member) {
         em.persist(member);
-        return member.getId();
     }
 
-    public Member find(Long id) {
+    public Member findOne(Long id) {
         return em.find(Member.class, id);
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    public List<Member> findByName(String name) {
+        return em.createQuery("select m from Member m where m.name = :name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
     }
 
 }
---
+++ jpaProject3/jpashop/src/test/java/com/maiorem/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,63 @@
+package com.maiorem.jpashop.service;
+
+import com.maiorem.jpashop.domain.Member;
+import com.maiorem.jpashop.repository.MemberRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@Transactional
+class MemberServiceTest {
+
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+    @Autowired EntityManager em;
+
+    @Test
+    @Rollback(value = false)
+    public void testJoin() throws Exception {
+        //given
+        Member member = new Member();
+        member.setName("kim");
+
+        //when
+        Long saveId = memberService.join(member);
+
+        //then
+//        em.flush();
+        assertEquals(member, memberRepository.findOne(saveId));
+
+    }
+
+    @Test
+    public void testValidationDuplicateMember() throws Exception {
+        //given
+        Member member1 = new Member();
+        member1.setName("lee");
+
+        Member member2 = new Member();
+        member2.setName("lee");
+
+        //when
+        memberService.join(member1);
+        try {
+            memberService.join(member2); //
+        } catch (IllegalStateException e) {
+            return;
+        }
+        //then
+        fail("Not Work.....");
+
+    }
+
+}
