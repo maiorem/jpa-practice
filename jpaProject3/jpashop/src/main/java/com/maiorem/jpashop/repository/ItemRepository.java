1089458bc705bf3fe2e501e1b405a9bfb465b2b8
Seyoung Hong <maiorem00@gmail.com>
2022-02-06 18:29:10 +0900

    Item Repository

---
+++ jpaProject3/jpashop/src/main/java/com/maiorem/jpashop/repository/ItemRepository.java
@@ -0,0 +1,32 @@
+package com.maiorem.jpashop.repository;
+
+import com.maiorem.jpashop.domain.item.Item;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+
+    private final EntityManager em;
+
+    public void save(Item item) {
+        if(item.getId() == null) {
+            em.persist(item); //없는 값이면 신규 등록
+        } else {
+            em.merge(item); //존재했던 상품이면 병합하여 업데이트
+        }
+    }
+
+    public Item findOne(Long id) {
+        return em.find(Item.class, id);
+    }
+
+    public List<Item> findAll() {
+        return em.createQuery("select i from Item i", Item.class)
+                .getResultList();
+    }
+}
