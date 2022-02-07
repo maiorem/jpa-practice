60c8bf49ed5c55f646e65107451afddb20dfe60f
Seyoung Hong <maiorem00@gmail.com>
2022-02-07 22:25:29 +0900

    Update OrderItem.java

--- jpaProject3/jpashop/src/main/java/com/maiorem/jpashop/domain/OrderItem.java
+++ jpaProject3/jpashop/src/main/java/com/maiorem/jpashop/domain/OrderItem.java
@@ -25,6 +25,18 @@ public class OrderItem {
     private int orderPrice; //주문가격
     private int count; //주문수량
 
+    //====생성 메서드====//
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count); //재고 차감
+        return orderItem;
+    }
+
+
     //====비즈니스 로직====//
     public void cancel() {
         getItem().addStock(count); //재고수량 원복
