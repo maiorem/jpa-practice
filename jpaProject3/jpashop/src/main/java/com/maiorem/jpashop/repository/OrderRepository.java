3eaf90dd7977d326a450b06683643fd43d332cb3
Seyoung Hong <maiorem00@gmail.com>
2022-02-09 20:51:10 +0900

    Update OrderRepository.java

--- jpaProject3/jpashop/src/main/java/com/maiorem/jpashop/repository/OrderRepository.java
+++ jpaProject3/jpashop/src/main/java/com/maiorem/jpashop/repository/OrderRepository.java
@@ -14,20 +14,16 @@ public class OrderRepository {
     private final EntityManager em;
 
     public void save(Order order) {
-        if(order.getId() == null) {
-            em.persist(order); //없는 값이면 신규 등록
-        } else {
-            em.merge(order); //존재했던 주문이면 병합하여 업데이트
-        }
+       em.persist(order);
     }
 
     public Order findOne(Long id) {
         return em.find(Order.class, id);
     }
 
-    public List<Order> findAll() {
-        return em.createQuery("select o from Order o", Order.class)
-                .getResultList();
-    }
+//    public List<Order> findAll() {
+//        return em.createQuery("select o from Order o", Order.class)
+//                .getResultList();
+//    }
 
 }
