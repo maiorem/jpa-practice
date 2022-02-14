2c17dc11869eda0d4b2dc49e2b2db201b4c82fa1
Seyoung Hong <maiorem00@gmail.com>
2022-02-14 19:40:58 +0900

    member form

---
+++ jpaProject3/jpashop/src/main/java/com/maiorem/jpashop/controller/MemberController.java
@@ -0,0 +1,21 @@
+package com.maiorem.jpashop.controller;
+
+import com.maiorem.jpashop.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/members/new")
+    public String createForm(Model model) {
+        model.addAttribute("memberForm", new MemberForm());
+        return "members/createMemberForm";
+    }
+
+}
