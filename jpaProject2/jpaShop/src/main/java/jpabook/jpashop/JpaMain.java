6fdec021ef4f556992c707a3ade31338655c5efa
Seyoung Hong <maiorem00@gmail.com>
2022-02-15 10:01:53 +0900

    N:M mapping

--- jpaProject2/jpaShop/src/main/java/jpabook/jpashop/JpaMain.java
+++ jpaProject2/jpaShop/src/main/java/jpabook/jpashop/JpaMain.java
@@ -1,5 +1,8 @@
 package jpabook.jpashop;
 
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
@@ -15,6 +18,8 @@ public class JpaMain {
 
         try {
 
+            Order order = new Order();
+            order.addOrderItem(new OrderItem());
 
 
             tx.commit();
