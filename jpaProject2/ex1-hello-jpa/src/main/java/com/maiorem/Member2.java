0d1dfe8e5b0ecd7eefa07a8aaed9e444b90b8de6
Seyoung Hong <maiorem00@gmail.com>
2022-02-22 15:02:04 +0900

    1:1

--- jpaProject2/ex1-hello-jpa/src/main/java/com/maiorem/Member2.java
+++ jpaProject2/ex1-hello-jpa/src/main/java/com/maiorem/Member2.java
@@ -25,6 +25,10 @@ public class Member2 {
     @JoinColumn(name = "TEAM_ID", insertable = false, updatable = false)
     private Team team;
 
+    @OneToOne
+    @JoinColumn(name = "LOCKER_ID")
+    private Locker locker;
+
     public Long getId() {
         return id;
     }
