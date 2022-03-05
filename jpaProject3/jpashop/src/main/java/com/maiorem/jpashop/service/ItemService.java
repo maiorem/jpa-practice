55da198c72f487b4c680fcfc554d91eca9eb81ee
Seyoung Hong <maiorem00@gmail.com>
2022-03-05 21:18:06 +0900

    변경감지

--- jpaProject3/jpashop/src/main/java/com/maiorem/jpashop/service/ItemService.java
+++ jpaProject3/jpashop/src/main/java/com/maiorem/jpashop/service/ItemService.java
@@ -24,14 +24,15 @@ public class ItemService {
     //변경감지 기능 사용(영속성 컨텍스트에서 엔티티를 조회한 후 데이터 수정)
     //==> 트랜잭션 커밋 시점에 변경감지 동작하여 update sql 실행
     @Transactional
-    public void updateItem(Long itemId, Book bookParam) {
+    public void updateItem(Long itemId, String name, int price, int stockQuantity) {
         Item findItem = itemRepository.findOne(itemId);
-        findItem.setPrice(bookParam.getPrice());
-        findItem.setName(bookParam.getName());
-        findItem.setStockQuantity(bookParam.getStockQuantity());
+        //findItem.change(price, name, stockQuantity); => 주로 쓰는 방식. 하나의 기능으로 추적할 수 있어야 함.
+        findItem.setPrice(price);
+        findItem.setName(name);
+        findItem.setStockQuantity(stockQuantity);
     }
 
-    //병합
+    //병합merge => (실무X) : 일부 수정을 못함. 전체를 갈아내야 해서 실무에서는 위험.
     @Transactional
     public Item updateItemMerge(Long itemId, Book bookParam) {
         Item findItem = itemRepository.findOne(itemId);
