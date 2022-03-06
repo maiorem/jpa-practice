65b6dbc40bf3c85d9c2b7000c05a11b62a5fb70a
Seyoung Hong <maiorem00@gmail.com>
2022-03-06 18:07:01 +0900

    order form

---
+++ jpaProject3/jpashop/src/main/java/com/maiorem/jpashop/controller/OrderController.java
@@ -0,0 +1,35 @@
+package com.maiorem.jpashop.controller;
+
+import com.maiorem.jpashop.domain.Member;
+import com.maiorem.jpashop.domain.item.Item;
+import com.maiorem.jpashop.service.ItemService;
+import com.maiorem.jpashop.service.MemberService;
+import com.maiorem.jpashop.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderService orderService;
+    private final MemberService memberService;
+    private final ItemService itemService;
+
+    @GetMapping("/order")
+    public String createForm(Model model) {
+        List<Member> members = memberService.findMembers();
+        List<Item> items = itemService.findItems();
+
+        model.addAttribute("members", members);
+        model.addAttribute("items", items);
+
+        return "order/orderForm";
+    }
+
+
+}
