f7c3ecc4a02f05a2fc52a74a1e946b3015426a8f
Seyoung Hong <maiorem00@gmail.com>
2022-03-10 20:57:04 +0900

    api

--- jpaProject3/jpashop/src/main/java/com/maiorem/jpashop/api/MemberApiController.java
+++ jpaProject3/jpashop/src/main/java/com/maiorem/jpashop/api/MemberApiController.java
@@ -16,13 +16,30 @@ public class MemberApiController {
 
     private final MemberService memberService;
 
+    //엔티티를 바로 쓰면 장애가 나기 쉬움
     @PostMapping("/api/v1/members")
     public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
         Long id = memberService.join(member);
         return new CreateMemberResponse(id);
 
     }
+    
+    //따라서 별도의 전송 객체를 생성해 사용
+    @PostMapping("/api/v2/members")
+    public CreateMemberResponse saveMEmberV2(@RequestBody @Valid CreateMemberRequest request) {
 
+        Member member = new Member();
+        member.setName(request.getName());
+
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+
+    }
+
+    @Data
+    static class CreateMemberRequest {
+        private String name;
+    }
 
     @Data
     static class CreateMemberResponse {
