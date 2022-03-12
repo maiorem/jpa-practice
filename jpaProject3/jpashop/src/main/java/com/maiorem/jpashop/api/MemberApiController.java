676324ffc2e8ca35a3ceccd503d7a8611983643f
Seyoung Hong <maiorem00@gmail.com>
2022-03-12 22:58:46 +0900

    member update api

--- jpaProject3/jpashop/src/main/java/com/maiorem/jpashop/api/MemberApiController.java
+++ jpaProject3/jpashop/src/main/java/com/maiorem/jpashop/api/MemberApiController.java
@@ -2,11 +2,10 @@ package com.maiorem.jpashop.api;
 
 import com.maiorem.jpashop.domain.Member;
 import com.maiorem.jpashop.service.MemberService;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
@@ -35,6 +34,25 @@ public class MemberApiController {
         Long id = memberService.join(member);
         return new CreateMemberResponse(id);
 
+    }
+    
+    @PutMapping("/api/v2/members/{id}")
+    public UpdateMemberResponse updateMemberV2(@PathVariable("id") Long id, @RequestBody @Valid UpdateMemberRequest request) {
+        memberService.update(id, request.getName());
+        Member findMember = memberService.findOne(id);
+        return new UpdateMemberResponse(findMember.getId(), findMember.getName());
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class UpdateMemberResponse {
+        private Long id;
+        private String name;
+    }
+
+    @Data
+    static class UpdateMemberRequest {
+        private String name;
     }
 
     @Data
