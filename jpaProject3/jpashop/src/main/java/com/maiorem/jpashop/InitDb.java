fb66c8b53c554fd13fc27da8db927623c56bc370
Seyoung Hong <maiorem00@gmail.com>
2022-03-15 18:31:38 +0900

    sample data update

--- jpaProject3/jpashop/src/main/java/com/maiorem/jpashop/InitDb.java
+++ jpaProject3/jpashop/src/main/java/com/maiorem/jpashop/InitDb.java
@@ -23,19 +23,20 @@ import javax.persistence.EntityManager;
 @RequiredArgsConstructor
 public class InitDb {
 
+    private final InitService initService;
+
+    @PostConstruct
+    public void init(){
+        initService.dbInit1();
+    }
+
     @Component
     @Transactional
     @RequiredArgsConstructor
     static class InitService {
 
-        private final InitService initService;
-
-        @PostConstruct
-        public void init(){
-            initService.dbInit1();
-        }
-
         private final EntityManager em;
+
         public void dbInit1() {
             Member member = new Member();
             member.setName("userA");
