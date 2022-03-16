6e55868078ea191ca7b93a1d6ed1ace4ea8b0107
Seyoung Hong <maiorem00@gmail.com>
2022-03-16 21:44:06 +0900

    Update OrderSimpleApiController.java

--- jpaProject3/jpashop/src/main/java/com/maiorem/jpashop/api/OrderSimpleApiController.java
+++ jpaProject3/jpashop/src/main/java/com/maiorem/jpashop/api/OrderSimpleApiController.java
@@ -22,9 +22,18 @@ public class OrderSimpleApiController {
 
     private final OrderRepository orderRepository;
 
+    // 연관관계가 있는 객체들이 지연로딩 상태이므로 ByteBuddyIntercepter에서 객체를 생성하여 에러 발생
+    // -> Hibernate5 modules 라이브러리 등록 (디폴트 : 지연로딩 무시)
+    // 엔티티 직접 노출로 인한 위험문제
     @GetMapping("/api/v1/simple-orders")
     public List<Order> ordersV1() {
         List<Order> all = orderRepository.findAllByString(new OrderSearch());
+
+        //전체 강제지연로딩을 끄고 원하는 객체만 지연로딩 하는 법
+        for (Order order : all) {
+            order.getMember().getName(); // LAZY 강제 초기화
+            order.getDelivery().getAddress();
+        }
         return all;
     }
 
