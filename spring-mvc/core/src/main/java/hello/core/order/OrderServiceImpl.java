d1d83e4714fdeb1e382d23655688aa3bb9f6f06f
Seyoung Hong <maiorem00@gmail.com>
2022-05-26 16:55:17 +0900

    외부에 의존성 주입으로 관심사 분리

--- spring-mvc/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ spring-mvc/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -9,12 +9,23 @@ import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService{
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+//    private final MemberRepository memberRepository = new MemoryMemberRepository();
 //    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
 //    private final DiscountPolicy discountPolicy = new RateDiscountPolicy(); // 새할인정책을 도입하면서 클라이언트를 수정함.. OCP DIP 원칙 준수X
-    ////인터페이스에만 의존하도록 해야 한다.////
-    private DiscountPolicy discountPolicy;  // <- 그러나 아무 값도 할당이 되어 있지 않아 null임
+    //======인터페이스에만 의존하도록 해야 한다.======//
+//    private DiscountPolicy discountPolicy;  // <- 그러나 아무 값도 할당이 되어 있지 않아 null임
                                             // 이 문제를 해결하려면 무언가가 OrderServiceImple에 DiscountPolicy의 구현 객체를 대신 생성하고 주입해 주어야 한다.
+
+
+    // 인터페이스에만 의존하는 설계
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
+
+    // 생성자를 통한 주입(연결)
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
--- spring-mvc/core/src/test/java/hello/core/member/MemberServiceTest.java
+++ spring-mvc/core/src/test/java/hello/core/member/MemberServiceTest.java
@@ -1,11 +1,21 @@
 package hello.core.member;
 
+import hello.core.AppConfig;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class MemberServiceTest {
 
-    MemberService memberService = new MemberServiceImpl();
+
+    MemberService memberService;
+
+    //각 테스트 실행 전에 무조건 실행되는 함수
+    @BeforeEach
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+    }
 
     @Test
     void join() {
