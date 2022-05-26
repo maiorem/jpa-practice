fc98002d2680110693bfea3b217aae97c967dd6b
Seyoung Hong <maiorem00@gmail.com>
2022-05-26 15:33:57 +0900

    순수 자바 코드 정책변경의 문제점

--- spring-mvc/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ spring-mvc/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
@@ -9,7 +10,11 @@ import hello.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+//    private final DiscountPolicy discountPolicy = new RateDiscountPolicy(); // 새할인정책을 도입하면서 클라이언트를 수정함.. OCP DIP 원칙 준수X
+    ////인터페이스에만 의존하도록 해야 한다.////
+    private DiscountPolicy discountPolicy;  // <- 그러나 아무 값도 할당이 되어 있지 않아 null임
+                                            // 이 문제를 해결하려면 무언가가 OrderServiceImple에 DiscountPolicy의 구현 객체를 대신 생성하고 주입해 주어야 한다.
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
