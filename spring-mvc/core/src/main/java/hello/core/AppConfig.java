8d9f9127734e2b1a36764f6f7286af553c6e50f3
Seyoung Hong <maiorem00@gmail.com>
2022-06-02 14:41:55 +0900

    discount change policy

--- spring-mvc/core/src/main/java/hello/core/AppConfig.java
+++ spring-mvc/core/src/main/java/hello/core/AppConfig.java
@@ -2,6 +2,7 @@ package hello.core;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -24,7 +25,8 @@ public class AppConfig {
     }
 
     public DiscountPolicy discountPolicy() {
-        return new FixDiscountPolicy();
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy(); //할인 정책 변경이 간편함. 구현 영역의 코드는 변경할 필요가 전혀 없음
     }
 
 }
