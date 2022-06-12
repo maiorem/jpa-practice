6d57f8022679881734854d72600f2763bfe81a41
Seyoung Hong <maiorem00@gmail.com>
2022-06-12 19:11:10 +0900

    auto component scan test

--- spring-mvc/core/src/main/java/hello/core/AutoAppConfig.java
+++ spring-mvc/core/src/main/java/hello/core/AutoAppConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
 @Configuration
-@ComponentScan(excludeFilters = @Filter(type = FilterType.ANNOTATION, classes = Configuration.class)) //@Component가 붙은 모든 클래스를 찾아 빈으로 자동 등록 / 제외 : Configuration 어노테이션
+@ComponentScan(excludeFilters = @Filter(type = FilterType.ANNOTATION, classes = Configuration.class)) //@Component가 붙은 모든 클래스를 찾아 빈으로 자동 등록 / 제외 : Configuration 어노테이션(테스트 용 클래스가 없다면 원래는 필요 없음)
 public class AutoAppConfig {
 
 }
