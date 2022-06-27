de9005b5e9c42198fbe07ec8cf947be248d422af
Seyoung Hong <maiorem00@gmail.com>
2022-06-27 15:56:46 +0900

    autowired

--- spring-mvc/core/src/main/java/hello/core/AutoAppConfig.java
+++ spring-mvc/core/src/main/java/hello/core/AutoAppConfig.java
@@ -1,7 +1,11 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScan.*;
@@ -14,10 +18,21 @@ public class AutoAppConfig {
 // basePackages = ""  ==> 시작되는 패키지 위치 지정. 이 하위 패키지만 컴포넌트스캔을 적용하도록 지정
 // basePackageClasses = "" ==> 해당되는 클래스의 패키지를 컴포넌트스캔의 시작위치로 적용하도록 지정. 디폴트는 설정 클래스의 패키지 (설정 정보가 있는 클래스를 때문에 프로젝트 최상단에 두는 것이 보통)
 // 일반적으로 서비스, 레포지토리, 컨트롤러 등 어노테이션은 컴포넌트로 자동 등록 되어있음
-    
+
+
+    // 필드주입 테스트용
+//    @Autowired MemberRepository memberRepository;
+//    @Autowired
+//    DiscountPolicy discountPolicy;
+//    @Bean
+//    OrderService orderService(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+//        return new OrderServiceImpl(memberRepository, discountPolicy);
+//    }
+//
 //    // 중복등록 테스트용 (수동 빈 등록)
 //    // 수동 빈이 우선권을 가지기 때문에 중복이 나도 자동 빈을 오버라이딩 해버리지만 부트는 중복 오류를 내도록 기본 설정 하고 있음.
-//    @Bean(name = "memoryMemberRepository")
+////    @Bean(name = "memoryMemberRepository")
+//    @Bean
 //    MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
 //    }
--- spring-mvc/core/src/test/java/hello/core/order/OrderServiceTest.java
+++ spring-mvc/core/src/test/java/hello/core/order/OrderServiceTest.java
@@ -1,10 +1,8 @@
 package hello.core.order;
 
 import hello.core.AppConfig;
-import hello.core.member.Grade;
-import hello.core.member.Member;
-import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.*;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -31,4 +29,14 @@ public class OrderServiceTest {
         Assertions.assertEquals(order.getDiscountPrice(), 1000);
     }
 
+//    @Test
+//    void fieldInjectionTest() {
+//        OrderServiceImpl orderService = new OrderServiceImpl();
+//
+//        orderService.setMemberRepository(new MemoryMemberRepository());
+//        orderService.setDiscountPolicy(new FixDiscountPolicy());
+//
+//        orderService.createOrder(1L, "itemA", 10000);
+//
+//    }
 }
