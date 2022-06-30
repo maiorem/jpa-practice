3d77e4a45f96f2983f23d19034768b32ece4e8e8
Seyoung Hong <maiorem00@gmail.com>
2022-06-30 17:10:49 +0900

    primary 적용

--- spring-mvc/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ spring-mvc/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -44,11 +44,11 @@ public class OrderServiceImpl implements OrderService{
     // 생성자를 통한 주입(연결). 생성자가 하나면 Autowired 없어도 됨
     // RequiredArgsConstructor 와 중복. 해당 롬복이 있으면 지워도 됨
     // 자동 주입 되는 빈 컴포넌트가 두개인 경우 Qualifier로 지정 가능
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
+//    @Autowired
+//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//    }
 
 
     // 일반 메서드를 통한 주입
