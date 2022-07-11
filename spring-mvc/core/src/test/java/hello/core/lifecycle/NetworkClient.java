1284a45bd3c47516b377257c2907b265db30431e
Seyoung Hong <maiorem00@gmail.com>
2022-07-11 14:14:43 +0900

    빈 초기화, 소멸 인터페이스

--- spring-mvc/core/src/test/java/hello/core/lifecycle/NetworkClient.java
+++ spring-mvc/core/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,13 +1,15 @@
 package hello.core.lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
 
     private String url;
 
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
-        connect();;
-        call("초기화 연결 메시지");
+
     }
 
     public void setUrl(String url) {
@@ -28,4 +30,18 @@ public class NetworkClient {
         System.out.println("close : " + url);
     }
 
+    // InitializingBean 초기화 인터페이스
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("NetworkClient.afterPropertiesSet");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    // DisposableBean 소멸 인터페이스
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("NetworkClient.destroy");
+        disConnect();
+    }
 }
